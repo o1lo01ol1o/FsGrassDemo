b25c1c32fc4b488a7a82c944e38903b3f77cc5ea
tpierson <tpierosn@aodedc.com>
2016-11-29 19:16:21 -0500

    Initall Commit

---
+++ FSharpDemo/FSharpDemoInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace FSharpDemo
+{
+    public class FSharpDemoInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "FSharpDemo";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("93546d4b-cd7b-446c-b18b-0d639eb160d9");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
---
+++ FsharpModules/Library1.fs
@@ -0,0 +1,58 @@
+﻿namespace FsharpModules
+open Grasshopper //<--These methods are accessable so long as you have a reference to grasshopper.dll etc
+open Rhino
+
+module Main =
+    //val main : string
+    let main = "this would be the main function"
+
+
+module Interpolation =
+
+    type CezanneOnRubens = 
+        | A 
+        | B 
+        | C 
+        | D 
+        | E 
+        | F 
+        | G 
+        | H 
+        | I 
+
+    // This is a tree structure
+    type Lineage = 
+        | Pater of CezanneOnRubens // it's either the root (above)
+        | Patrimony of Lineage * Lineage  // or it's the product of two lineages
+
+    type Individual = Set<CezanneOnRubens> // individual is the sum of parents
+
+    // recursively count generations given a lineage
+    let rec getGenerationNo (t:Lineage) (g:int) = // note "rec" in funciton binding
+        match t with
+        | Pater root -> g
+        | Patrimony (maternal, _) -> getGenerationNo maternal (g + 1)
+
+    //Get the ancestors
+    let rec givenName (t:Lineage) (l: Set<CezanneOnRubens>) =
+        match t with
+        | Pater root -> Set.add root Set.empty
+        | Patrimony (m, p) -> let mat = givenName m l
+                              let pat = givenName p l
+                              Set.union mat pat
+        
+
+    type Drawing = Rhino.Geometry.Curve list
+
+    type APointInCORSpace = 
+        {
+        Drawing: Drawing; // You'll have to leave this empty until you make the new drawing
+        Identity: Lineage
+        }
+
+    let fornicate (a : APointInCORSpace) (b:APointInCORSpace ) = // and yields a new childs
+        let newchild = "implent me here"
+        newchild // <-- return this value
+
+ 
+
